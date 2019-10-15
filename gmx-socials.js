e756eb6bcf5c11510a133a10700b1f5a614a6058
GMX9 <41052152+GMX9@users.noreply.github.com>
2019-10-15 15:21:17 +0100

    Javascript ES6 Arrow Function Update
    Update to main core file to the new JS ES6 Arrow Functions

--- gmx-socials.js
+++ gmx-socials.js
@@ -1,55 +1,71 @@
-"use strict";
+class gmx_socials{
 
-// Generate Social Buttons
-function generateSocialButtons(socialLink,socialClass,socialIcon,socialTitle){
-    
-    // Default button
-    var defButton = '<a href="'+socialLink+'"><button type="button" class="btn btn-secondary social '+socialClass+'"><i class="'+socialIcon+'"></i> '+socialTitle+'</button></a>';
-    
-    $(".gmx_inline-socials").append(defButton);
-    
-}    
+    constructor(entry){
 
-// Generate needed styles
-function styleSheet(){
-    
-    var fontawesome = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">';
-    var stylesheet = '<style>.social{ color:white; float:left; margin-right:10px; padding-right:18px; padding-top:11px; padding-bottom:11px; padding-left:18px; border-radius:0px; border:0px; transition: all .4s ease; -webkit-transition: all .4s ease; } .social:hover{ margin-top:-15px; } .social i{ margin-right:10px; } .facebook{ background:#3b5999 !important; } .twitter{ background:#55acee !important; } .linkedin{ background:#0077B5 !important; } .telegram{ background:#0088cc !important; } .tumblr{ background:#00405d !important; } .pinterest{ background:#bd081c !important; } .vk{ background:#45668e !important; } .whatsapp{ background:#43d854 !important; }</style>';
-    
-    $("head").append(fontawesome);
-    $("head").append(stylesheet);
-}
+        this.styleSheet();
 
-// Create the ICON
-function socialIcon(socialNetwork){
-        
-    var socialNetwork = socialNetwork.toLowerCase();    
-        
-    var s_Facebook = "fab fa-facebook-f";     
-    var s_Twitter = "fab fa-twitter";
-    var s_Linkedin = "fab fa-linkedin-in";
-    var s_Tumblr = "fab fa-tumblr";
-    var s_VK = "fab fa-vk";
-    var s_WhatsApp = "fab fa-whatsapp";
-    var s_Pinterest = "fab fa-pinterest-p";
-    var s_Telegram = "fab fa-telegram-plane";
-    
-    var s_Social = "";
-    
-    if(socialNetwork == "facebook"){ s_Social = s_Facebook;  }
-    if(socialNetwork == "twitter"){ s_Social = s_Twitter;  }
-    if(socialNetwork == "linkedin"){ s_Social = s_Linkedin;  }
-    if(socialNetwork == "tumblr"){ s_Social = s_Tumblr;  }
-    if(socialNetwork == "vk"){ s_Social = s_VK;  }
-    if(socialNetwork == "whatsapp"){ s_Social = s_WhatsApp;  }
-    if(socialNetwork == "pinterest"){ s_Social = s_Pinterest;  }
-    if(socialNetwork == "telegram"){ s_Social = s_Telegram;  }
-    
-    return s_Social;
+        socials.forEach((entry) => {
+                
+            var socialNetwork = entry;
+            var socialLink = window.location.href;
+
+            this.generateSocialButtons(this.socialLinks(socialNetwork,socialLink),this.socialClass(socialNetwork),this.socialIcon(socialNetwork),this.socialTitle(socialNetwork));
+            console.log("Generating..");
             
-}
+        });
 
-function socialClass(socialNetwork){
+    }
+
+    // Generate Social Buttons
+    generateSocialButtons(socialLink,socialClass,socialIcon,socialTitle){
+        
+        // Default button
+        var defButton = '<a href="'+socialLink+'"><button type="button" class="btn btn-secondary social '+socialClass+'"><i class="'+socialIcon+'"></i> '+socialTitle+'</button></a>';
+        
+        $(".gmx_inline-socials").append(defButton);
+        
+    }    
+
+    // Generate needed styles
+    styleSheet(){
+        
+        var fontawesome = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">';
+        var stylesheet = '<style>.social{ color:white; float:left; margin-right:10px; padding-right:18px; padding-top:11px; padding-bottom:11px; padding-left:18px; border-radius:0px; border:0px; transition: all .4s ease; -webkit-transition: all .4s ease; } .social:hover{ margin-top:-15px; } .social i{ margin-right:10px; } .facebook{ background:#3b5999 !important; } .twitter{ background:#55acee !important; } .linkedin{ background:#0077B5 !important; } .telegram{ background:#0088cc !important; } .tumblr{ background:#00405d !important; } .pinterest{ background:#bd081c !important; } .vk{ background:#45668e !important; } .whatsapp{ background:#43d854 !important; }</style>';
+        
+        $("head").append(fontawesome);
+        $("head").append(stylesheet);
+    }
+
+    // Create the ICON
+    socialIcon(socialNetwork){
+            
+        var socialNetwork = socialNetwork.toLowerCase();    
+            
+        var s_Facebook = "fab fa-facebook-f";     
+        var s_Twitter = "fab fa-twitter";
+        var s_Linkedin = "fab fa-linkedin-in";
+        var s_Tumblr = "fab fa-tumblr";
+        var s_VK = "fab fa-vk";
+        var s_WhatsApp = "fab fa-whatsapp";
+        var s_Pinterest = "fab fa-pinterest-p";
+        var s_Telegram = "fab fa-telegram-plane";
+        
+        var s_Social = "";
+        
+        if(socialNetwork == "facebook"){ s_Social = s_Facebook;  }
+        if(socialNetwork == "twitter"){ s_Social = s_Twitter;  }
+        if(socialNetwork == "linkedin"){ s_Social = s_Linkedin;  }
+        if(socialNetwork == "tumblr"){ s_Social = s_Tumblr;  }
+        if(socialNetwork == "vk"){ s_Social = s_VK;  }
+        if(socialNetwork == "whatsapp"){ s_Social = s_WhatsApp;  }
+        if(socialNetwork == "pinterest"){ s_Social = s_Pinterest;  }
+        if(socialNetwork == "telegram"){ s_Social = s_Telegram;  }
+        
+        return s_Social;
+                
+    }
+
+    socialClass(socialNetwork){
         
     var socialNetwork = socialNetwork.toLowerCase();    
         
@@ -75,74 +91,65 @@ function socialClass(socialNetwork){
     
     return s_Social;
             
-}    
+    }    
 
-function socialLinks(socialNetwork,socialLink){
-    
-    var socialNetwork = socialNetwork.toLowerCase();    
+    socialLinks(socialNetwork,socialLink){
         
-    var s_Facebook = "http://www.facebook.com/sharer.php?u=";     
-    var s_Twitter = "http://www.linkedin.com/shareArticle?mini=true&amp;url=";
-    var s_Linkedin = "https://twitter.com/share?url=";
-    var s_Tumblr = "http://www.tumblr.com/share/link?url=";
-    var s_VK = "http://vkontakte.ru/share.php?url=";
-    var s_Pinterest = "http://pinterest.com/pin/create/link/?url=";
-    var s_WhatsApp = "whatsapp://send?text="; // Only works for mobile
-    var s_Telegram = "tg://share?url="; // Only works for mobile
-    
-    var s_Social = "";
-    
-    if(socialNetwork == "facebook"){ s_Social = s_Facebook;  }
-    if(socialNetwork == "twitter"){ s_Social = s_Twitter;  }
-    if(socialNetwork == "linkedin"){ s_Social = s_Linkedin;  }
-    if(socialNetwork == "tumblr"){ s_Social = s_Tumblr;  }
-    if(socialNetwork == "vk"){ s_Social = s_VK;  }
-    if(socialNetwork == "whatsapp"){ s_Social = s_WhatsApp;  }
-    if(socialNetwork == "pinterest"){ s_Social = s_Pinterest;  }
-    if(socialNetwork == "telegram"){ s_Social = s_Telegram;  }
-    
-    s_Social = s_Social+socialLink;
-    
-    return s_Social;   
-    
-}
-
-function socialTitle(socialNetwork){
-        
-    var socialNetwork = socialNetwork.toLowerCase();    
-        
-    var s_Facebook = "Facebook";     
-    var s_Twitter = "Twitter";
-    var s_Linkedin = "Linkedin";
-    var s_Tumblr = "Tumblr";
-    var s_VK = "VK";
-    var s_WhatsApp = "WhatsApp";
-    var s_Pinterest = "Pinterest";    
-    var s_Telegram = "Telegram";
-    
-    var s_Social = "";
-    
-    if(socialNetwork == "facebook"){ s_Social = s_Facebook;  }
-    if(socialNetwork == "twitter"){ s_Social = s_Twitter;  }
-    if(socialNetwork == "linkedin"){ s_Social = s_Linkedin;  }
-    if(socialNetwork == "tumblr"){ s_Social = s_Tumblr;  }
-    if(socialNetwork == "vk"){ s_Social = s_VK;  }
-    if(socialNetwork == "whatsapp"){ s_Social = s_WhatsApp;  }
-    if(socialNetwork == "pinterest"){ s_Social = s_Pinterest;  }
-    if(socialNetwork == "telegram"){ s_Social = s_Telegram;  }
-    
-    return s_Social;
+        var socialNetwork = socialNetwork.toLowerCase();    
             
-}   
-
-styleSheet();
-
-socials.forEach(function(entry) {
+        var s_Facebook = "http://www.facebook.com/sharer.php?u=";     
+        var s_Twitter = "http://www.linkedin.com/shareArticle?mini=true&amp;url=";
+        var s_Linkedin = "https://twitter.com/share?url=";
+        var s_Tumblr = "http://www.tumblr.com/share/link?url=";
+        var s_VK = "http://vkontakte.ru/share.php?url=";
+        var s_Pinterest = "http://pinterest.com/pin/create/link/?url=";
+        var s_WhatsApp = "whatsapp://send?text="; // Only works for mobile
+        var s_Telegram = "tg://share?url="; // Only works for mobile
         
-    var socialNetwork = entry;
-    var socialLink = window.location.href;
+        var s_Social = "";
+        
+        if(socialNetwork == "facebook"){ s_Social = s_Facebook;  }
+        if(socialNetwork == "twitter"){ s_Social = s_Twitter;  }
+        if(socialNetwork == "linkedin"){ s_Social = s_Linkedin;  }
+        if(socialNetwork == "tumblr"){ s_Social = s_Tumblr;  }
+        if(socialNetwork == "vk"){ s_Social = s_VK;  }
+        if(socialNetwork == "whatsapp"){ s_Social = s_WhatsApp;  }
+        if(socialNetwork == "pinterest"){ s_Social = s_Pinterest;  }
+        if(socialNetwork == "telegram"){ s_Social = s_Telegram;  }
+        
+        s_Social = s_Social+socialLink;
+        
+        return s_Social;   
+        
+    }
 
-    generateSocialButtons(socialLinks(socialNetwork,socialLink),socialClass(socialNetwork),socialIcon(socialNetwork),socialTitle(socialNetwork));
-    console.log("Generating..");
-    
-});
+    socialTitle(socialNetwork){
+            
+        var socialNetwork = socialNetwork.toLowerCase();    
+            
+        var s_Facebook = "Facebook";     
+        var s_Twitter = "Twitter";
+        var s_Linkedin = "Linkedin";
+        var s_Tumblr = "Tumblr";
+        var s_VK = "VK";
+        var s_WhatsApp = "WhatsApp";
+        var s_Pinterest = "Pinterest";    
+        var s_Telegram = "Telegram";
+        
+        var s_Social = "";
+        
+        if(socialNetwork == "facebook"){ s_Social = s_Facebook;  }
+        if(socialNetwork == "twitter"){ s_Social = s_Twitter;  }
+        if(socialNetwork == "linkedin"){ s_Social = s_Linkedin;  }
+        if(socialNetwork == "tumblr"){ s_Social = s_Tumblr;  }
+        if(socialNetwork == "vk"){ s_Social = s_VK;  }
+        if(socialNetwork == "whatsapp"){ s_Social = s_WhatsApp;  }
+        if(socialNetwork == "pinterest"){ s_Social = s_Pinterest;  }
+        if(socialNetwork == "telegram"){ s_Social = s_Telegram;  }
+        
+        return s_Social;
+                
+    }   
+
+
+}
